526d73cf0e710f2ae4eaaff99fd70efcff209998
Scyibearian <cyberboy02@gmail.com>
2024-11-16 17:09:12 +0000

    Implemented landing page

---
+++ components/index.jsx
@@ -0,0 +1,29 @@
+
+import CustomButton from "./CustomButton";
+
+
+export {
+
+  CustomButton,
+
+};
+
+// import VideoCard from "./VideoCard";
+// import FormField from "./FormField";
+// import CustomButton from "./CustomButton";
+// import InfoBox from "./InfoBox";
+// import Loader from "./Loader";
+// import Trending from "./Trending";
+// import SearchInput from "./SearchInput";
+// import EmptyState from "./EmptyState";
+
+// export {
+//   VideoCard,
+//   FormField,
+//   CustomButton,
+//   InfoBox,
+//   Loader,
+//   Trending,
+//   SearchInput,
+//   EmptyState,
+// };
